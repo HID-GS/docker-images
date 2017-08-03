9b1aca30080e7cf5ce8ed9e3e1e70ab2a65abfb9
Peter Carrero <peter.carrero@gmail.com>
2017-08-03 11:08:15 -0500

    nginx: Actually change entrypoint

--- tools-nginx/init/entrypoint.sh
+++ tools-nginx/init/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 cd /etc/nginx/conf.d
 # Delete old configs
 find . -type f -name '*.conf' -delete
@@ -6,9 +6,37 @@ find . -type f -name '*.conf' -delete
 for file in {.,}*;
 do
   if ping -c 1 ${file%.conf.*} &> /dev/null
-	then
+  then
     cp ${file} ${file%.tpl}
-	fi
+  fi
 done
 # Start nginx
-nginx -g "daemon off;"
+nginx
+
+while [ 1 -eq 1]; do
+
+  read -t 60
+
+  restart=0
+
+  ls *.conf.tpl | while read template; do
+    config=$(echo $template | sed 's/.tpl//g')
+    host=$(echo $template | sed 's/.conf.tpl//g')
+    ping -c 1 $host &> /dev/null
+    if [ $? -eq 1 ]; then
+      if [ !-f $config ]; then
+        cp $template $config
+        restart=1
+      fi
+    else if [ -f $config ]; then
+      rm $config
+      restart=1
+    fi
+  done
+
+  if [ $restart -eq 1 ]; then
+    #restart nginx
+    nginx -s reload
+  fi
+
+done
