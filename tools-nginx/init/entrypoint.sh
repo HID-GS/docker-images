e5e228f80345cc06aabcf80c6428daa49f7b1d0a
Peter Carrero <peter.carrero@gmail.com>
2017-12-11 15:15:10 -0600

    Fix typo on entrypoint

--- tools-nginx/init/entrypoint.sh
+++ tools-nginx/init/entrypoint.sh
@@ -122,9 +122,9 @@ semaphore_check_freshness() {
     age_limit=3600
     if [ $file_age -gt $age_limit ]; then
       log_text "Found stale semaphore file. Killing it."
-      log_test "semaphore age: $file_timestamp"
-      log_test "now: $now"
-      log_test "now: $(date)"
+      log_text "semaphore age: $file_timestamp"
+      log_text "now: $now"
+      log_text "now: $(date)"
       rm -f ${semaphore}
     fi
   fi
